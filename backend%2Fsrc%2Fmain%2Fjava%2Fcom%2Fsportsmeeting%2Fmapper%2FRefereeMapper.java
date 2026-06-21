1bedc15b995cfab0cf0d8b1cd593c18e5c1302ee
wanganfudge <hehangyu7004@163.com>
2026-06-21 15:16:42 +0800

    Upload backend/src/main/java/com/sportsmeeting/mapper/RefereeMapper.java

---
+++ backend%2Fsrc%2Fmain%2Fjava%2Fcom%2Fsportsmeeting%2Fmapper%2FRefereeMapper.java
@@ -0,0 +1,6 @@
+package com.sportsmeeting.mapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.sportsmeeting.entity.Referee;
+import org.apache.ibatis.annotations.Mapper;
+@Mapper
+public interface RefereeMapper extends BaseMapper<Referee> {}
