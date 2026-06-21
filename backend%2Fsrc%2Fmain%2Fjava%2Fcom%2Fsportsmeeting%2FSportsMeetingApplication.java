67aa9bde099b4350315e935fe01c40fa0ed427f0
wanganfudge <hehangyu7004@163.com>
2026-06-21 15:17:09 +0800

    Upload backend/src/main/java/com/sportsmeeting/SportsMeetingApplication.java

---
+++ backend%2Fsrc%2Fmain%2Fjava%2Fcom%2Fsportsmeeting%2FSportsMeetingApplication.java
@@ -0,0 +1,13 @@
+package com.sportsmeeting;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.sportsmeeting.mapper")
+public class SportsMeetingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SportsMeetingApplication.class, args);
+    }
+}
