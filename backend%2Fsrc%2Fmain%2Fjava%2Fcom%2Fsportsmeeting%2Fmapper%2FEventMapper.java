e6033dbc8642deb0351b2bf7db1c78ac46b8f047
wanganfudge <hehangyu7004@163.com>
2026-06-21 15:16:39 +0800

    Upload backend/src/main/java/com/sportsmeeting/mapper/EventMapper.java

---
+++ backend%2Fsrc%2Fmain%2Fjava%2Fcom%2Fsportsmeeting%2Fmapper%2FEventMapper.java
@@ -0,0 +1,6 @@
+package com.sportsmeeting.mapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.sportsmeeting.entity.Event;
+import org.apache.ibatis.annotations.Mapper;
+@Mapper
+public interface EventMapper extends BaseMapper<Event> {}
