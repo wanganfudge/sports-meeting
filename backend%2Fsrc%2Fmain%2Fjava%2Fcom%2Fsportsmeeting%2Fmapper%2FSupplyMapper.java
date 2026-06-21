6d9a3d66ab85a95c69d95b1331f0fde4416d9473
wanganfudge <hehangyu7004@163.com>
2026-06-21 15:16:45 +0800

    Upload backend/src/main/java/com/sportsmeeting/mapper/SupplyMapper.java

---
+++ backend%2Fsrc%2Fmain%2Fjava%2Fcom%2Fsportsmeeting%2Fmapper%2FSupplyMapper.java
@@ -0,0 +1,6 @@
+package com.sportsmeeting.mapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.sportsmeeting.entity.Supply;
+import org.apache.ibatis.annotations.Mapper;
+@Mapper
+public interface SupplyMapper extends BaseMapper<Supply> {}
