647bed0a94f4fa505e30708c4f041015039fa010
wanganfudge <hehangyu7004@163.com>
2026-06-21 15:16:49 +0800

    Upload backend/src/main/java/com/sportsmeeting/service/DutyScheduleService.java

---
+++ backend%2Fsrc%2Fmain%2Fjava%2Fcom%2Fsportsmeeting%2Fservice%2FDutyScheduleService.java
@@ -0,0 +1,4 @@
+package com.sportsmeeting.service;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.sportsmeeting.entity.DutySchedule;
+public interface DutyScheduleService extends IService<DutySchedule> {}
