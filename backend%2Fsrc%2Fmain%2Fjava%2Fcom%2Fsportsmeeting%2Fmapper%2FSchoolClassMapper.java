5645d4900f0b192b11bfc2e82153cb7f3a205bbe
wanganfudge <hehangyu7004@163.com>
2026-06-21 15:16:43 +0800

    Upload backend/src/main/java/com/sportsmeeting/mapper/SchoolClassMapper.java

---
+++ backend%2Fsrc%2Fmain%2Fjava%2Fcom%2Fsportsmeeting%2Fmapper%2FSchoolClassMapper.java
@@ -0,0 +1,6 @@
+package com.sportsmeeting.mapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.sportsmeeting.entity.SchoolClass;
+import org.apache.ibatis.annotations.Mapper;
+@Mapper
+public interface SchoolClassMapper extends BaseMapper<SchoolClass> {}
