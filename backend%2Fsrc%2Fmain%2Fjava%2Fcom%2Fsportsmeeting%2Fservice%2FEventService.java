5ea5f21e8ecb29e5d97c8f3d8d28d13fcca75ca6
wanganfudge <hehangyu7004@163.com>
2026-06-21 15:16:50 +0800

    Upload backend/src/main/java/com/sportsmeeting/service/EventService.java

---
+++ backend%2Fsrc%2Fmain%2Fjava%2Fcom%2Fsportsmeeting%2Fservice%2FEventService.java
@@ -0,0 +1,4 @@
+package com.sportsmeeting.service;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.sportsmeeting.entity.Event;
+public interface EventService extends IService<Event> {}
