3c086b90ed206980828f8eaa305e3bd98f21bacf
wanganfudge <hehangyu7004@163.com>
2026-06-21 15:16:38 +0800

    Upload backend/src/main/java/com/sportsmeeting/mapper/DutyScheduleMapper.java

---
+++ backend%2Fsrc%2Fmain%2Fjava%2Fcom%2Fsportsmeeting%2Fmapper%2FDutyScheduleMapper.java
@@ -0,0 +1,6 @@
+package com.sportsmeeting.mapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.sportsmeeting.entity.DutySchedule;
+import org.apache.ibatis.annotations.Mapper;
+@Mapper
+public interface DutyScheduleMapper extends BaseMapper<DutySchedule> {}
