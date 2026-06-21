4bbbb84f947bc031126f18ff8bbb06b4da1d60f8
wanganfudge <hehangyu7004@163.com>
2026-06-21 15:16:40 +0800

    Upload backend/src/main/java/com/sportsmeeting/mapper/ParticipationMapper.java

---
+++ backend%2Fsrc%2Fmain%2Fjava%2Fcom%2Fsportsmeeting%2Fmapper%2FParticipationMapper.java
@@ -0,0 +1,6 @@
+package com.sportsmeeting.mapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.sportsmeeting.entity.Participation;
+import org.apache.ibatis.annotations.Mapper;
+@Mapper
+public interface ParticipationMapper extends BaseMapper<Participation> {}
